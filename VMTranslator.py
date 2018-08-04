9e543ba8415ba5dd51ad81b2450a8cf0fbed18fc
Brian Cunnie <brian.cunnie@gmail.com>
2018-08-04 07:01:01 -0700

    VMTranslator is a glorified `cat`

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,12 +1,35 @@
-#!/usr/bin/python
+#!/usr/local/bin/python2.7
 import sys
 
-if len(sys.argv) != 1
-    sys.exit("Error I love muh dawg")
+
+def parse(line):
+    return(line)
+
+def writecode(parsed_data):
+    print parsed_data
+
+cmd_name = sys.argv[0].split('/')[-1]
+
+if len(sys.argv) != 2:
+    sys.exit(cmd_name + " error: pass me one arg, the name of the file to compile")
+
+intermediate_filename = sys.argv[1]
+
 try:
-    in_file = open(sys.argv[1], "r")
+    intermediate_code = open(intermediate_filename, "r")
 except:
-    sys.exit("ERROR. Did you make a mistake in the spelling")
-text = in_file.read()
-print text
-in_file.close()
+    sys.exit(cmd_name + " error. I couldn't open " + intermediate_filename + " for reading!")
+
+asm_filename = intermediate_filename.replace('.vm', '.asm')
+
+if intermediate_filename == asm_filename:
+    asm_filename += ".asm"
+
+try:
+    asm_code = open(asm_filename, "w")
+except:
+    sys.exit(cmd_name + " error. I couldn't open " + asm_filename + " for writing!")
+
+for line in intermediate_code:
+    x = parse(line)
+    writecode(x)

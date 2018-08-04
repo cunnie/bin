035188b9357b71d30b772ec5f62e53132c5fa3b0
Brian Cunnie <brian.cunnie@gmail.com>
2018-08-04 11:01:55 -0700

    Add now works

--- VMTranslator.py
+++ VMTranslator.py
@@ -1,12 +1,70 @@
 #!/usr/local/bin/python2.7
 import sys
 
+t_cmd = [
+    'push',
+    'pop',
+    'add',
+    'sub',
+    'neg',
+    'eq',
+    'gt',
+    'lt',
+    'and',
+    'or',
+    'not'
+]
+
+t_segment = [
+    'argument',
+    'local',
+    'static',
+    'constant',
+    'this',
+    'that',
+    'pointer',
+    'temp',
+]
+
 
 def parse(line):
-    return(line)
+    # strip comments
+    no_comments = line.split('#', 1)[0]
+    no_comments = no_comments.split('//', 1)[0]
+    tokens = no_comments.split()
+    parsed_tokens = tokens
+    if len(parsed_tokens) > 0:
+        parsed_tokens[0] = t_cmd.index(tokens[0])
+    if len(parsed_tokens) > 1:
+        parsed_tokens[1] = t_segment.index(tokens[1])
+    # parsed_tokens[2] is already set
+    return parsed_tokens
 
-def writecode(parsed_data):
-    print parsed_data
+
+def writecode(tokens):
+    if not tokens:
+        return
+    asm_file.write('//')
+    for token in tokens:
+        asm_file.write(' ' + str(token))
+    asm_file.write('\n')
+    if t_cmd[tokens[0]] == 'add':
+        asm_file.write('@SP\n')
+        asm_file.write('M=M-1\n')
+        asm_file.write('A=M\n')
+        asm_file.write('D=M	// D is value at top of the stack\n')
+        asm_file.write('@SP\n')
+        asm_file.write('A=M-1	// Point to next highest on stack\n')
+        asm_file.write('M=D+M	// Add D to that guy\n')
+    if t_cmd[tokens[0]] == 'push':
+        if t_segment[tokens[1]] == 'constant':
+            asm_file.write('@' + tokens[2] + '\n')
+            asm_file.write('D=A\n')
+            asm_file.write('@SP\n')
+            asm_file.write('A=M\n')
+            asm_file.write('M=D\n')
+            asm_file.write('@SP\n')
+            asm_file.write('M=M+1\n')
 
 cmd_name = sys.argv[0].split('/')[-1]
 
@@ -26,7 +84,7 @@ if intermediate_filename == asm_filename:
     asm_filename += ".asm"
 
 try:
-    asm_code = open(asm_filename, "w")
+    asm_file = open(asm_filename, "w")
 except:
     sys.exit(cmd_name + " error. I couldn't open " + asm_filename + " for writing!")
 

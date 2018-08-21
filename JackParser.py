b317190fee687d9b2bce52ba950b50e93a36ec02
Brian Cunnie <brian.cunnie@gmail.com>
2018-08-21 06:55:29 -0700

    Nand2Tetris: parser, a stake in the ground (skeleton)

---
+++ JackParser.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python2.7
+import fileinput
+import glob
+import os
+import sys
+import datetime
+
+
+class JackAnalyzer:
+    def __init__(self):
+        return
+
+
+class JackTokenizer:
+    def __init__(self):
+        return
+
+
+class JackTokenizer:
+    def __init__(self):
+        return
+
+
+print("I love my dog")

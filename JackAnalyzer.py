29fa0bc4f2769a08f3a9dc1aeb359b7c67b1e92f
Brian Cunnie <brian.cunnie@gmail.com>
2018-08-22 18:17:29 -0700

    Re-wrote to accommodate suggested API
    The API is described on page 214 (Chapter10, the _JackTokenizer_
    Module):
    
    - includes the definitions of methods such as
      - `hasMoreTokens()`
      - `advance()`
      - `tokenType()`
      - `keyWord()`  # only applies to keyword tokens
      - etc...

--- JackAnalyzer.py
+++ JackAnalyzer.py
@@ -9,15 +9,27 @@ import datetime
 
 class JackAnalyzer:
     def __init__(self, source, dest):
-        self.source = source
         self.dest = dest
-        self.tokenizer = JackTokenizer()
+        self.tokenizer = JackTokenizer(source)
         return
 
-    def tokenize(self):
+    def xml(self):
         self.dest.write('<tokens>\n')
-        for line in self.source:
-            self.dest.write(self.tokenizer.tokenize(line))
+        for token in self.tokenizer.tokens:
+            if token.type == Token.KEYWORD:
+                self.dest.write('<keyword> ' + token.keyWord + ' </keyword>')
+            elif token.type == Token.SYMBOL:
+                self.dest.write('<symbol> ' + escapeSymbol(token.symbol) + ' </symbol>')
+            elif token.type == Token.IDENTIFIER:
+                self.dest.write('<identifier> ' + token.identifier + ' </identifier>')
+            elif token.type == Token.INT_CONST:
+                self.dest.write('<integerConstant> ' + str(token.intVal) + ' </integerConstant>')
+            elif token.type == Token.STRING_CONST:
+                self.dest.write('<stringConstant> ' + token.stringVal + ' </stringConstant>')
+            else:
+                sys.stderr.write("I know not this token: \"" + token.type + "\"\n")
+                sys.exit(3)
+            self.dest.write('\n')
         self.dest.write('</tokens>\n')
 
 
@@ -29,31 +41,72 @@ class JackTokenizer:
     symbols = '{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', \
               '/', '&', ',', '<', '>', '=', '~'
     reSymbols = re.compile(r'([{\}()\[\],.;+\-*/&<>=~])')
+    reIdentifier = re.compile(r'^[a-zA-Z]{1}[a-zA-Z0-9]*$')
     reIntegerConstant = re.compile(r'^\d+$')
+    reStringConstant = re.compile(r'^".*"$')
 
-    def __init__(self):
+    def __init__(self, source):
+        self.source = source
+        self.token = None
+        self.tokens = self.readAllTokens()
         return
 
-    def tokenize(self, line):
-        xml = ''
-        bigFields = line.split()
+    def hasMoreTokens(self):
+        if len(self.tokens) > 0:
+            return True
+        else:
+            return False
+
+    def advance(self):
+        self.token = self.tokens.pop(0)
+
+    def tokenType(self):
+        return self.token.type
+
+    def symbol(self):
+        return self.token.symbol
+
+    def identifier(self):
+        return self.token.identifier
+
+    def intVal(self):
+        return self.token.intVal
+
+    def stringVal(self):
+        return self.token.stringVal
+
+    def readAllTokens(self):
+        tokens = []
+        input = self.source.read()
+        input = re.sub(r'//.*$', '', input, flags=re.MULTILINE)  # strip comments `//`
+        input = re.sub(r'/\*.*?\*/', '', input, flags=re.MULTILINE)  # strip comments `/* */`
+        bigFields = input.split()
         for bigField in bigFields:
             fields = JackTokenizer.reSymbols.split(bigField)
             for field in fields:
+                # sys.stderr.write("field: \"" + field + "\"\n")
                 # Lame coding alert: I shouldn't have empty strings
                 # and I shouldn't skip them by using `pass`. This
                 # code is double-lame, but I'll never fix it, sorry.
                 if field == '':
                     pass
                 elif field in JackTokenizer.keywords:
-                    xml += '  <keyword>' + field + '</keyword>\n'
+                    tokens.append(Token(Token.KEYWORD, keyWord=field))  # += '  <keyword>' + field + '</keyword>\n'
                 elif field in JackTokenizer.symbols:
-                    xml += '  <symbol>' + escapeSymbol(field) + '</symbol>\n'
+                    tokens.append(
+                        Token(Token.SYMBOL, symbol=field))  # xml += '  <symbol>' + escapeSymbol(field) + '</symbol>\n'
+                elif JackTokenizer.reIdentifier.match(field):
+                    tokens.append(Token(Token.IDENTIFIER, identifier=field))
                 elif JackTokenizer.reIntegerConstant.match(field):
-                    xml += '  <integerConstant>' + escapeSymbol(field) + '</integerConstant>\n'
+                    tokens.append(Token(Token.INT_CONST, intVal=int(
+                        field)))  # xml += '  <integerConstant>' + escapeSymbol(field) + '</integerConstant>\n'
+                elif JackTokenizer.reStringConstant.match(field):
+                    tokens.append(Token(Token.STRING_CONST, stringVal=re.sub(r'^"?(.*?)"?$', r'\1',
+                                                                             field)))  # xml += '  <identifier>' + re.sub(r'^"?(.*?)"?$', r'\1', field) + '</identifier>\n'
                 else:
-                    xml += '  <identifier>' + re.sub(r'^"?(.*?)"?$', r'\1', field) + '</identifier>\n'
-        return (xml)
+                    sys.stderr.write("I can't figure out this field: \"" + field + "\"!")
+                    sys.exit(1)
+        return (tokens)
 
 
 # Ensure XML-dangerous symbols are properly escaped
@@ -64,6 +117,47 @@ def escapeSymbol(symbol):
         '"': '&quot;',
         '&': '&amp;',
     }.get(symbol, symbol)
+
+
+class Token:
+    def __init__(self, type, keyWord=None, symbol=None, identifier=None, intVal=None, stringVal=None):
+        self.type = type
+        self.keyWord = keyWord
+        self.symbol = symbol
+        self.symbol = symbol
+        self.identifier = identifier
+        self.intVal = intVal
+        self.stringVal = stringVal
+
+    # Token Types
+    KEYWORD = 1
+    SYMBOL = 2
+    IDENTIFIER = 3
+    INT_CONST = 4
+    STRING_CONST = 5
+
+    # Key Words (for Token Type KEYWORD)
+    CLASS = 10
+    METHOD = 11
+    FUNCTION = 12
+    CONSTRUCTOR = 13
+    INT = 14
+    BOOLEAN = 15
+    CHAR = 16
+    VOID = 17
+    VAR = 18
+    STATIC = 19
+    FIELD = 20
+    LET = 21
+    DO = 22
+    IF = 23
+    ELSE = 24
+    WHILE = 25
+    RETURN = 26
+    TRUE = 27
+    FALSE = 28
+    NULL = 29
+    THIS = 30
 
 
 class CompilationEngine:
@@ -110,4 +204,4 @@ for sourceFileName in jackSourceFilenames():
     sys.stderr.write(cmd_name + ": opened " + destFileName + " for writing.\n")
 
     analyzer = JackAnalyzer(source, dest)
-    analyzer.tokenize()
+    analyzer.xml()

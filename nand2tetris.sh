d3e3a915038ba3fd23c15dda0c81eb461d30dded
Brian Cunnie <brian.cunnie@gmail.com>
2018-09-05 15:07:35 -0700

    Regression tests for Jack Compiler

--- nand2tetris.sh
+++ nand2tetris.sh
@@ -2,15 +2,17 @@
 set -eu
 
 pushd ~/Desktop/nand2tetris/projects/11/Seven
-rm Main.vm; JackCompiler.py Main.jack; vimdiff Main{,X}.vm
+rm -f Main.vm; JackCompiler.py; vimdiff Main{,X}.vm
 popd
 pushd ~/Desktop/nand2tetris/projects/11/ConvertToBin/
-rm Main.vm; JackCompiler.py Main.jack; vimdiff Main{,X}.vm
+rm -f Main.vm; JackCompiler.py; vimdiff Main{,X}.vm
 popd
 pushd ~/Desktop/nand2tetris/projects/11/Square/
-rm Main.vm Squqre.vm SquareGame.vm
-JackCompiler.py
+rm -f Main.vm Square.vm SquareGame.vm; JackCompiler.py
 vimdiff Main{,X}.vm
 vimdiff Square{,X}.vm
 vimdiff SquareGame{,X}.vm
+popd
+pushd ~/Desktop/nand2tetris/projects/11/Average/
+rm -f Main.vm; JackCompiler.py; vimdiff Main{,X}.vm
 popd

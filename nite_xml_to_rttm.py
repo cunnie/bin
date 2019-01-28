671eff082d61321c9eaee408bad995093a910863
Brian Cunnie <brian.cunnie@gmail.com>
2019-01-27 16:53:17 -0800

    NITE XML -> RTTM
    Currently it only prints "hello world"

---
+++ nite_xml_to_rttm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# This program takes a series of XML-formatted files and creates an
+# RTTM v1.3 (https://catalog.ldc.upenn.edu/docs/LDC2004T12/RTTM-format-v13.pdf)
+# output file.
+
+# Typical invocation:
+#
+#   nite_xml_to_rttm.py ~/Downloads/ami_public_manual_1.6.2/words/ES2008a.*.words.xml > /tmp/ES2008a.rttm
+
+print("hello world!")
+
+

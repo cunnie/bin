9a70354204f0856a2b9c31893f9179aec58338a3
Brian Cunnie <bcunnie@pivotal.io>
2019-07-27 06:41:57 -0700

    listener binds to 8080 and prints message

--- listener.go
+++ listener.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net"
 )
 
 func main() {
-	fmt.Println("vim-go")
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		// handle error
@@ -14,8 +12,14 @@ func main() {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			// handle error
+			panic("Oh dear, I couldn't Accept()")
 		}
-		go handleConnection(conn)
+		// go handleConnection(conn)
+		_, err = conn.Write([]byte("vim-go\n"))
+		err = conn.Close()
+		if err != nil {
+			panic("Oh dear, I couldn't Close()")
+		}
+
 	}
 }

e88f49aced4a615c17c89cb2ce0bcba7f56228c8
Brian Cunnie <bcunnie@pivotal.io>
2019-09-14 17:20:55 -0700

    Listener listens to a port and spits back the info

--- listener.go
+++ listener.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net"
 )
 
+// main binds to a port and sends back the Remote Address and the local address
+// FIXME: I don't think this works
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
@@ -15,7 +18,7 @@ func main() {
 			panic("Oh dear, I couldn't Accept()")
 		}
 		// go handleConnection(conn)
-		_, err = conn.Write([]byte("vim-go\n"))
+		_, err = conn.Write([]byte(fmt.Sprintf("%s\n%s\n", conn.RemoteAddr(), ln.Addr())))
 		err = conn.Close()
 		if err != nil {
 			panic("Oh dear, I couldn't Close()")

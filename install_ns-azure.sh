48f2cfbd4b9b86b8ee8fd45d0afaee51c716e61e
Brian Cunnie <brian.cunnie@gmail.com>
2022-04-27 08:34:02 -0700

    ns-{aws,azure}: copy web assets from updated location
    fixes:
    ```
    rsync: [sender] change_dir "/home/cunnie/workspace/sslip.io/k8s/document_root" failed: No such file or directory (2)
    ```

--- install_ns-azure.sh
+++ install_ns-azure.sh
@@ -266,7 +266,7 @@ install_sslip_io_web() {
   HTML_DIR=/var/nginx/sslip.io
   if [ ! -d $HTML_DIR ]; then
     sudo mkdir -p $HTML_DIR
-    sudo rsync -avH ~/workspace/sslip.io/k8s/document_root/ $HTML_DIR/
+    sudo rsync -avH ~/workspace/sslip.io/k8s/document_root_sslip.io/ $HTML_DIR/
     sudo chown -R $USER $HTML_DIR
     sudo chmod -R g+w $HTML_DIR # so I can write acme certificate information
     sudo curl -L https://raw.githubusercontent.com/cunnie/deployments/master/terraform/aws/sslip.io-vm/sslip.io.nginx.conf \

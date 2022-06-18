b294b554f6f07880f312f707474303fad7b79e35
Brian Cunnie <brian.cunnie@gmail.com>
2022-06-18 15:10:32 -0700

    DNS PTRs: update to new subnet, 10.9.9.0/24

--- a_to_ptr.rb
+++ a_to_ptr.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 
 # typical use:
-# dig @nono.io axfr nono.io. | a_to_ptr.rb > /usr/local/etc/namedb/master/9.0.10.in-addr.arpa
+# dig @nono.io axfr nono.io. | a_to_ptr.rb > /usr/local/etc/namedb/master/9.9.10.in-addr.arpa
 
 puts <<EOF
 $TTL 3h
-@ SOA vain.nono.io. yoyo.nono.io. #{Time.now.to_i} 1d 12h 1w 3h
+@ SOA atom.nono.io. yoyo.nono.io. #{Time.now.to_i} 1d 12h 1w 3h
         ; Serial, Refresh, Retry, Expire, Neg. cache TTL
 
-        NS      vain.nono.io.
+        NS      atom.nono.io.
 EOF
 
 octet_to_record = {}
@@ -16,7 +16,7 @@ STDIN.read.split("\n").each do |line|
   if line.match?(/\tA\t/)
     fields = line.split(" ")
     # p fields
-    if fields[4].match?(/^10\.0\.9\./)
+    if fields[4].match?(/^10\.9\.9\./)
       octet = fields[4].split(".")[3].to_i
       octet_to_record[octet] = "#{octet}\tPTR\t#{fields[0]}"
     end

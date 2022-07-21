4c2c97ee17c9030e095ff2a4cb60f94471be9f7e
Brian Cunnie <brian.cunnie@gmail.com>
2022-07-21 13:28:37 -0400

    Even smaller (42k→16k) avx executable

--- avx.c
+++ avx.c
@@ -1,6 +1,3 @@
-#include "stdio.h"
-
-void main() {
-    printf("This program will generate an \"Illegal instruction\" and \"trap invalid opcode\" on machines without avx.\n");
+int main() {
     asm ("VZEROALL"); // an AVX instruction, https://en.wikipedia.org/wiki/Advanced_Vector_Extensions
 }

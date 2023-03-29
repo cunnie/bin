053f562fd78143ee4c7441e4aa73c4867ade71d2
Brian Cunnie <brian.cunnie@gmail.com>
2023-03-29 16:33:59 -0700

    a_to_ptr: placate Rubocop

--- a_to_ptr.rb
+++ a_to_ptr.rb
@@ -3,23 +3,23 @@
 # typical use:
 # dig @nono.io axfr nono.io. | a_to_ptr.rb > /usr/local/etc/namedb/master/9.9.10.in-addr.arpa
 
-puts <<EOF
-$TTL 3h
-@ SOA atom.nono.io. yoyo.nono.io. #{Time.now.to_i} 1d 12h 1w 3h
-        ; Serial, Refresh, Retry, Expire, Neg. cache TTL
+puts <<~AXFR
+  $TTL 3h
+  @ SOA atom.nono.io. yoyo.nono.io. #{Time.now.to_i} 1d 12h 1w 3h
+          ; Serial, Refresh, Retry, Expire, Neg. cache TTL
 
-        NS      atom.nono.io.
-EOF
+          NS      atom.nono.io.
+AXFR
 
 octet_to_record = {}
-STDIN.read.split("\n").each do |line|
-  if line.match?(/\tA\t/)
-    fields = line.split(" ")
-    # p fields
-    if fields[4].match?(/^10\.9\.9\./)
-      octet = fields[4].split(".")[3].to_i
-      octet_to_record[octet] = "#{octet}\tPTR\t#{fields[0]}"
-    end
+$stdin.read.split("\n").each do |line|
+  next unless line.match?(/\tA\t/)
+
+  fields = line.split(' ')
+  # p fields
+  if fields[4].match?(/^10\.9\.9\./)
+    octet = fields[4].split('.')[3].to_i
+    octet_to_record[octet] = "#{octet}\tPTR\t#{fields[0]}"
   end
 end
 

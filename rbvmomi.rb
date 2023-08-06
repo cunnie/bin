14be494051866c029a70ecf7b27da8f40b35a448
Brian Cunnie <brian.cunnie@gmail.com>
2023-08-06 08:07:25 -0700

    rbvmomi: diligently safeguard against `nil`
    ...to avoid the endless torrent of pages when I attempt a method on a
    nil object.

--- rbvmomi.rb
+++ rbvmomi.rb
@@ -17,15 +17,18 @@ vim = RbVmomi::VIM.connect(
   password: ENV['GOVC_PASSWORD']
 ) || raise("couldn't log in")
 dc = vim.serviceInstance.find_datacenter('dc') || raise('datacenter not found')
-cl = dc.find_compute_resource('cl')
-esxi1 = cl.host.select { |host| host.name == 'esxi-1.nono.io' }.first
-esxi2 = cl.host.select { |host| host.name == 'esxi-2.nono.io' }.first
-esxi1.config.graphicsConfig
-esxi1.configManager.graphicsManager.sharedPassthruGpuTypes # ["grid_t4-4q", etc.]
+cl = dc.find_compute_resource('cl') || raise('cluster not found')
+esxi1 = cl.host.select { |host| host.name == 'esxi-1.nono.io' }&.first
+esxi2 = cl.host.select { |host| host.name == 'esxi-2.nono.io' }&.first
+esxi1&.config&.graphicsConfig
+esxi1&.configManager&.graphicsManager&.sharedPassthruGpuTypes # ["grid_t4-4q", etc.]
+
+esxi1&.configManager&.graphicsManager&.graphicsInfo&.first
+# → HostGraphicsInfo( deviceName: "TU104GL [Tesla T4]", graphicsType: "direct", memorySizeInKB: 0, pciId: "0000:17:00.0", vendorName: "NVIDIA Corporation", vm: [] )
 # esxi1.config.pciPassthruInfo
-lunar = esxi2.vm.select { |vm| vm.name == 'lunar.nono.io' }.first
-devices = lunar.config.hardware.device
-pci_pass = devices.select { |dev| dev.instance_of?(RbVmomi::VIM::VirtualPCIPassthrough) }.first
-devices.delete(devices.index(pci_pass)) # <-- doesn't remove the device
+lunar = esxi1&.vm&.select { |vm| vm.name == 'lunar.nono.io' }&.first
+devices = lunar&.config&.hardware&.device
+# devices.delete(devices.index(pci_pass)) # <-- doesn't remove the device
 binding.pry
-devices.class
+pci_pass = devices&.select { |dev| dev.instance_of?(RbVmomi::VIM::VirtualPCIPassthrough) }&.first
+devices&.class

c2781742439728a1a135b1fdbaf8b7e8e6b41fec
Brian Cunnie <brian.cunnie@gmail.com>
2023-08-07 10:28:12 -0700

    rbvmomi: create a virtual PCI Passthrough device

--- rbvmomi.rb
+++ rbvmomi.rb
@@ -22,12 +22,16 @@ esxi1 = cl.host.select { |host| host.name == 'esxi-1.nono.io' }&.first
 esxi2 = cl.host.select { |host| host.name == 'esxi-2.nono.io' }&.first
 esxi1&.config&.graphicsConfig
 esxi1&.configManager&.graphicsManager&.sharedPassthruGpuTypes # ["grid_t4-4q", etc.]
-
 esxi1&.configManager&.graphicsManager&.graphicsInfo&.first
 # → HostGraphicsInfo( deviceName: "TU104GL [Tesla T4]", graphicsType: "direct", memorySizeInKB: 0, pciId: "0000:17:00.0", vendorName: "NVIDIA Corporation", vm: [] )
 # esxi1.config.pciPassthruInfo
 lunar = esxi1&.vm&.select { |vm| vm.name == 'lunar.nono.io' }&.first
 devices = lunar&.config&.hardware&.device
+
+allowedDevice = RbVmomi::VIM::VirtualPCIPassthroughAllowedDevice.new(deviceId: 7864, vendorId: 4318) # Nvidia Tesla T4
+backing = RbVmomi::VIM::VirtualPCIPassthroughDynamicBackingInfo.new(allowedDevice: [allowedDevice])
+virtualPCIPassthrough = RbVmomi::VIM::VirtualPCIPassthrough.new(backing: backing)
+
 # devices.delete(devices.index(pci_pass)) # <-- doesn't remove the device
 binding.pry
 pci_pass = devices&.select { |dev| dev.instance_of?(RbVmomi::VIM::VirtualPCIPassthrough) }&.first

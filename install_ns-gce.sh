bb6f5ab50ed3a1a650abfb6119cfefa15c11bd3d
Brian Cunnie <brian.cunnie@gmail.com>
2023-11-15 11:43:05 -0800

    Install ns-gce: Fedora, patterned after fed.nono.io

---
+++ install_ns-gce.sh
@@ -0,0 +1,217 @@
+#!/bin/bash -x
+set -eu -o pipefail
+
+install_packages() {
+  sudo dnf groupinstall -y "Development Tools"
+  sudo dnf install -y \
+    binutils \
+    btrfs-progs \
+    cmake \
+    cronie \
+    direnv \
+    dnf-plugins-core \
+    fd-find \
+    gcc-g++ \
+    git \
+    git-lfs \
+    golang \
+    golang-x-tools-gopls \
+    htop \
+    iproute \
+    iputils \
+    jq \
+    lastpass-cli \
+    libxml2-devel \
+    libcurl-devel \
+    mysql-devel \
+    neovim \
+    net-tools \
+    nmap-ncat \
+    npm \
+    openssl-devel \
+    postgresql-devel \
+    python3-pip \
+    redhat-rpm-config \
+    ripgrep \
+    ruby \
+    ruby-devel \
+    rubygems \
+    socat \
+    strace \
+    tcpdump \
+    tmux \
+    util-linux-user \
+    wget \
+    zlib-devel \
+    zsh \
+    zsh-lovers \
+    zsh-syntax-highlighting \
+
+}
+
+install_chruby() {
+  if [ ! -d /usr/local/share/chruby ] ; then
+    wget -O ruby-install-0.8.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.8.3.tar.gz
+    tar -xzvf ruby-install-0.8.3.tar.gz
+    cd ruby-install-0.8.3/
+    sudo make install
+
+    wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+    tar -xzvf chruby-0.3.9.tar.gz
+    cd chruby-0.3.9/
+    sudo make install
+    cat >> ~/.zshrc <<EOF
+
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+EOF
+  fi
+}
+
+install_fasd() {
+  if [ ! -x /usr/local/bin/fasd ]; then
+    cd ~/workspace
+    git clone git@github.com:clvv/fasd.git
+    cd fasd
+    sudo make install
+    cat >> ~/.zshrc <<EOF
+
+eval "\$(fasd --init posix-alias zsh-hook)"
+alias z='fasd_cd -d'     # cd, same functionality as j in autojump
+EOF
+  fi
+}
+
+install_bin() {
+  if [ ! -d $HOME/bin ]; then
+    git clone git@github.com:cunnie/bin.git $HOME/bin
+    echo 'PATH="$HOME/bin:$PATH:/usr/local/go/bin"' >> ~/.zshrc
+    ln -s ~/bin/env/git-authors ~/.git-authors
+  fi
+}
+
+
+install_terraform() {
+  if [ ! -x /usr/local/bin/terraform ]; then
+    curl -o tf.zip -L https://releases.hashicorp.com/terraform/1.6.3/terraform_1.6.3_linux_amd64.zip
+    unzip tf.zip
+    sudo install terraform /usr/local/bin/
+  fi
+}
+
+install_zsh_autosuggestions() {
+  if [ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+      git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+      sed -i 's/^plugins=(/&zsh-autosuggestions /' $HOME/.zshrc
+  fi
+}
+
+install_git_duet() {
+  if [ ! -x /usr/local/bin/git-duet ]; then
+    mkdir -p /tmp/$$/git-duet
+    pushd /tmp/$$
+    curl -o git-duet.tgz -L https://github.com/git-duet/git-duet/releases/download/0.9.0/linux_amd64.tar.gz
+    tar -xzvf git-duet.tgz -C git-duet/
+    sudo install git-duet/* /usr/local/bin
+    popd
+  fi
+}
+
+configure_direnv() {
+  if ! grep -q "direnv hook zsh" ~/.zshrc; then
+    echo 'eval "$(direnv hook zsh)"' >> ~/.zshrc
+    eval "$(direnv hook bash)"
+  fi
+  for envrc in $(find "$HOME/workspace" -maxdepth 2 -name '.envrc' -print); do
+    pushd $(dirname $envrc)
+      direnv allow
+    popd
+  done
+}
+
+configure_zsh() {
+  if [ ! -f $HOME/.zshrc ]; then
+    sudo chsh -s /usr/bin/zsh $USER
+    echo "" | SHELL=/usr/bin/zsh zsh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sed -i 's/robbyrussell/agnoster/' ~/.zshrc
+    echo 'eval "$(fasd --init posix-alias zsh-hook)"' >> ~/.zshrc
+    echo 'export EDITOR=nvim' >> ~/.zshrc
+    echo 'alias k=kubectl' >> ~/.zshrc
+    echo "# Don't log me out of LastPass for 10 hours" >> ~/.zshrc
+    echo 'export LPASS_AGENT_TIMEOUT=36000' >> ~/.zshrc
+    echo 'export USE_GKE_GCLOUD_AUTH_PLUGIN=True # fixes "WARNING: the gcp auth plugin is deprecated in v1.22+, unavailable in v1.25+;' >> ~/.zshrc
+    echo '. $HOME/.venv/base/bin/activate' >> ~/.zshrc
+  fi
+}
+
+use_pacific_time() {
+  sudo timedatectl set-timezone America/Los_Angeles
+}
+
+configure_git() {
+  # https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
+  git config --global user.name "Brian Cunnie"
+  git config --global user.email brian.cunnie@gmail.com
+  git config --global alias.co checkout
+  git config --global alias.ci commit
+  git config --global alias.st status
+  git config --global url."git@github.com:".insteadOf "https://github.com/"
+  git config --global color.branch auto
+  git config --global color.diff auto
+  git config --global color.status auto
+  git config --global core.editor nvim
+}
+
+configure_tmux() {
+  # https://github.com/luan/tmuxfiles, to clear, `rm -rf ~/.tmux.conf ~/.tmux`
+  if [ ! -f $HOME/.tmux.conf ]; then
+    echo "WARNING: If this scripts fails with \"unknown variable: TMUX_PLUGIN_MANAGER_PATH\""
+    echo "If you don't have an ugly magenta bottom of your tmux screen, if nvim is unusable, then"
+    echo "you may need to run this command to completely install tmux configuration:"
+    echo "zsh -c \"\$(curl -fsSL https://raw.githubusercontent.com/luan/tmuxfiles/master/install)\""
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/luan/tmuxfiles/master/install)"
+  fi
+}
+
+disable_firewalld() {
+  # so that BIND can work
+  sudo systemctl stop firewalld || true
+  sudo systemctl disable firewalld || true
+}
+
+configure_passwordless_sudo() {
+  SUDO_FILE=/etc/sudoers.d/passwordless
+  if ! sudo test -f $SUDO_FILE ; then
+    sudo tee $SUDO_FILE <<EOF
+# Ubuntu: Allow members of group sudo  to execute any command
+%sudo  ALL=(ALL) NOPASSWD: ALL
+# Fedora: Allow members of group wheel to execute any command
+%wheel ALL=(ALL) NOPASSWD: ALL
+EOF
+  fi
+}
+
+configure_python_venv() {
+  VENV_DIR=$HOME/.venv/base
+  if [ ! -d $VENV_DIR ]; then
+    python3 -m venv $VENV_DIR
+    source $VENV_DIR/bin/activate
+  fi
+}
+
+install_packages
+mkdir -p ~/workspace
+configure_zsh          # needs to come before install steps that modify .zshrc
+install_bin
+install_chruby
+install_fasd
+install_git_duet
+install_terraform
+install_zsh_autosuggestions
+use_pacific_time
+disable_firewalld
+configure_direnv
+configure_git
+configure_tmux
+configure_passwordless_sudo
+configure_python_venv

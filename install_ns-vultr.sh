105dfce12997113580ad539bd71f00e57d1848e7
Brian Cunnie <brian.cunnie@gmail.com>
2023-12-02 17:58:47 +0000

    Install ns-vultr: named works
    ...and named (BIND) is really what it's all about

--- install_ns-vultr.sh
+++ install_ns-vultr.sh
@@ -2,7 +2,7 @@
 set -eux -o pipefail
 
 install_packages() {
-  sudo pkg install -y \
+  pkg install -y \
     bash \
     bind918 \
     fasd \
@@ -36,13 +36,15 @@ adduser_cunnie() {
       -w random \
       -f <(echo "cunnie::::::Brian Cunnie::/usr/local/bin/zsh:")
     mkdir -p ~cunnie/.ssh
-    touch ~cunnie/.ssh/known_hosts
-    chmod -R go-rwx ~cunnie/.ssh
-    chown -R cunnie:cunnie ~cunnie/.ssh
     # pre-populate GitHub key to sidestep dialogue  "The authenticity of host ...can't be established."
     cat > ~cunnie/.ssh/known_hosts <<EOF
 github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
 EOF
+    cat > ~cunnie/.ssh/authorized_keys <<EOF
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWiAzxc4uovfaphO0QVC2w00YmzrogUpjAzvuqaQ9tD cunnie@nono.io
+EOF
+    chmod -R go-rwx ~cunnie/.ssh
+    chown -R cunnie:cunnie ~cunnie/.ssh
   fi
 }
 
@@ -71,16 +73,30 @@ configure_zsh() {
 }
 
 configure_bind() {
-  if ! sudo grep -q nono.io /etc/named.conf; then
-    sudo sed -i 's/listen-on port 53.*/listen-on port 53 { any; };/;
+  if ! grep -q nono.io /usr/local/etc/namedb/named.conf; then
+    sed -i '' 's/listen-on port 53.*/listen-on port 53 { any; };/;
       s/listen-on-v6 port 53.*/listen-on-v6 port 53 { any; };/;
-      s/allow-query.*/allow-query     { any; }; allow-query-cache { any; };/' /etc/named.conf
-    sudo tee -a /etc/named.conf << EOF
-zone "9.0.10.in-addr.arpa" {
+      s/allow-query.*/allow-query     { any; }; allow-query-cache { any; };/' /usr/local/etc/namedb/named.conf
+    cat >> /usr/local/etc/namedb/named.conf << EOF
+zone "diarizer.com" {
 	type slave;
-	file "9.0.10.in-addr.arpa";
+	file "diarizer.com";
 	masters {
-		2601:646:100:69f0::a; // atom.nono.io
+		2a01:4f8:c17:b8f::2; //shay.nono.io
+	};
+};
+zone "foundry.fun" {
+	type slave;
+	file "foundry.fun";
+	masters {
+		2a01:4f8:c17:b8f::2; //shay.nono.io
+	};
+};
+zone "nono.com" {
+	type slave;
+	file "nono.com";
+	masters {
+		2a01:4f8:c17:b8f::2; //shay.nono.io
 	};
 };
 zone "nono.io" {
@@ -91,15 +107,18 @@ zone "nono.io" {
 	};
 };
 EOF
-    sudo systemctl enable named-chroot
-    sudo systemctl start named-chroot
+    cat >> /etc/rc.conf <<EOF
+named_enable="YES"
+EOF
+    /usr/local/etc/rc.d/named start
   fi
 }
 
+[ $(id -u) = 0 ] || ( echo "I need to be run as root"; exit 1 )
 install_packages
 configure_passwordless_sudo
 adduser_cunnie
 configure_git
-configure_zsh          # needs to come before install steps that modify .zshrc
-exit
+configure_zsh
 configure_bind
+echo "remember to set the passwords for root & cunnie"

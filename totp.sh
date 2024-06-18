3a69d0763b73bc936f1ef345d53b748a9a9604de
Brian Cunnie <brian.cunnie@gmail.com>
2024-06-18 07:06:27 -0700

    totp: better comment at top for teammates

--- totp.sh
+++ totp.sh
@@ -3,7 +3,13 @@
 # Script to display QR codes containing TOTP secrets to be scanned in
 # by an authenticator app.
 #
+# Make sure you've done `brew install qrencode`
+#
 # invocation: totp.sh < file_of_urls_one_per_line
+#
+# Typical line:
+#
+#   otpauth://totp/GitHub:brian.cunnie@broadcom.com?secret=b2............f4
 #
 # e.g. lpass show --note totp.txt | totp.sh
 
